8312487e5957ed4aba515ca93a18f13cef3b3fb4
Mathieu Grauwin <mathieu.grauwin1@gmail.com>
2020-02-24 23:42:50 +0100

    Weather client to get data from API

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.weatherapp.services.DownloadImageTask;
+import com.example.weatherapp.services.WeatherClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,55 +36,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //TODO extract this initialization into a function
+        this.queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://www.prevision-meteo.ch/services/json/montpellier", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                WeatherClient weatherClient= new WeatherClient(response);
 
-        TextView currentTemperature = findViewById(R.id.current_temperature);
-        ImageView currentConditionIcon = findViewById(R.id.current_condition_icon);
-        TextView currentCondition = findViewById(R.id.current_condition);
+                //TODO extract this initialization into a function
 
-        currentTemperature.setText("32°");
-        new DownloadImageTask(currentConditionIcon).execute("https://www.prevision-meteo.ch/style/images/icon/ensoleille-big.png");
-        currentCondition.setText("Ensoleillé");
+                TextView currentTemperature = findViewById(R.id.current_temperature);
+                ImageView currentConditionIcon = findViewById(R.id.current_condition_icon);
+                TextView currentCondition = findViewById(R.id.current_condition);
 
-        View detailsCurrentWeatherGrid = findViewById(R.id.details_current_weather_grid);
+                currentTemperature.setText(weatherClient.getTemperature());
+                new DownloadImageTask(currentConditionIcon).execute(weatherClient.getIconCondition());
+                currentCondition.setText(weatherClient.getCondition());
 
-        View humidityView = detailsCurrentWeatherGrid.findViewById(R.id.humidity);
-        ImageView humidityIcon = humidityView.findViewById(R.id.detail_icon);
-        TextView humidityPercentage = humidityView.findViewById(R.id.detail_value);
-        TextView humidityLabel = humidityView.findViewById(R.id.detail_label);
+                View detailsCurrentWeatherGrid = findViewById(R.id.details_current_weather_grid);
 
-        humidityIcon.setImageResource(R.drawable.water);
-        humidityPercentage.setText("74 %");
-        humidityLabel.setText("Humidité");
+                View humidityView = detailsCurrentWeatherGrid.findViewById(R.id.humidity);
+                ImageView humidityIcon = humidityView.findViewById(R.id.detail_icon);
+                TextView humidityPercentage = humidityView.findViewById(R.id.detail_value);
+                TextView humidityLabel = humidityView.findViewById(R.id.detail_label);
 
+                humidityIcon.setImageResource(R.drawable.water);
+                humidityPercentage.setText(weatherClient.getHumidity());
+                humidityLabel.setText(R.string.humidity_label);
 
-        View pressureView = detailsCurrentWeatherGrid.findViewById(R.id.pressure);
-        ImageView pressureIcon = pressureView.findViewById(R.id.detail_icon);
-        TextView pressureValue = pressureView.findViewById(R.id.detail_value);
-        TextView pressureLabel = pressureView.findViewById(R.id.detail_label);
+                View pressureView = detailsCurrentWeatherGrid.findViewById(R.id.pressure);
+                ImageView pressureIcon = pressureView.findViewById(R.id.detail_icon);
+                TextView pressureValue = pressureView.findViewById(R.id.detail_value);
+                TextView pressureLabel = pressureView.findViewById(R.id.detail_label);
 
-        pressureIcon.setImageResource(R.drawable.meter);
-        pressureValue.setText("1018.6");
-        pressureLabel.setText("Pression atmosphérique");
+                pressureIcon.setImageResource(R.drawable.meter);
+                pressureValue.setText(weatherClient.getPressure());
+                pressureLabel.setText(R.string.pressure_label);
 
+                View windDirectionView = detailsCurrentWeatherGrid.findViewById(R.id.wind_direction);
+                ImageView windDirectionIcon = windDirectionView.findViewById(R.id.detail_icon);
+                TextView windDirectionValue = windDirectionView.findViewById(R.id.detail_value);
+                TextView windDirectionLabel = windDirectionView.findViewById(R.id.detail_label);
 
-        View windDirectionView = detailsCurrentWeatherGrid.findViewById(R.id.wind_direction);
-        ImageView windDirectionIcon = windDirectionView.findViewById(R.id.detail_icon);
-        TextView windDirectionValue = windDirectionView.findViewById(R.id.detail_value);
-        TextView windDirectionLabel = windDirectionView.findViewById(R.id.detail_label);
+                windDirectionIcon.setImageResource(R.drawable.compass);
+                windDirectionValue.setText(weatherClient.getWindDirection());
+                windDirectionLabel.setText(R.string.wind_direction_label);
 
-        windDirectionIcon.setImageResource(R.drawable.compass);
-        windDirectionValue.setText("E");
-        windDirectionLabel.setText("Direction du vent");
+                View uvIndexView = detailsCurrentWeatherGrid.findViewById(R.id.uv_index);
+                ImageView uvIndexIcon = uvIndexView.findViewById(R.id.detail_icon);
+                TextView uvIndexValue = uvIndexView.findViewById(R.id.detail_value);
+                TextView uvIndexLabel = uvIndexView.findViewById(R.id.detail_label);
 
-        View uvIndexView = detailsCurrentWeatherGrid.findViewById(R.id.uv_index);
-        ImageView uvIndexIcon = uvIndexView.findViewById(R.id.detail_icon);
-        TextView uvIndexValue = uvIndexView.findViewById(R.id.detail_value);
-        TextView uvIndexLabel = uvIndexView.findViewById(R.id.detail_label);
-
-        uvIndexIcon.setImageResource(R.drawable.uv);
-        uvIndexValue.setText("Très faible");
-        uvIndexLabel.setText("Indice UV");
-
+                uvIndexIcon.setImageResource(R.drawable.uv);
+                uvIndexValue.setText("Très faible");
+                uvIndexLabel.setText(R.string.uv_index_label);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("RequestError", error.getMessage());
+            }
+        });
+        this.queue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/services/WeatherClient.java
@@ -0,0 +1,106 @@
+package com.example.weatherapp.services;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class WeatherClient {
+    private String city;
+    private String sunrise;
+    private String sunset;
+    private String temperature;
+    private String maxTemperature;
+    private String minTemperature;
+    private String humidity;
+    private String pressure;
+    private String windDirection;
+    private String condition;
+    private String iconCondition;
+    private String dayShort;
+
+    public WeatherClient(String response) {
+        try {
+            JSONObject jsonResponse = new JSONObject(response);
+
+            JSONObject city = jsonResponse.getJSONObject("city_info");
+            this.city = city.getString("name");
+            this.sunrise = city.getString("sunrise");
+            this.sunset = city.getString("sunset");
+
+            JSONObject currentDay = jsonResponse.getJSONObject("fcst_day_0");
+            this.dayShort = currentDay.getString("day_short");
+            this.maxTemperature = currentDay.getString("tmax");
+            this.minTemperature = currentDay.getString("tmin");
+
+            JSONObject hourlyDataCurrentDay = currentDay.getJSONObject("hourly_data");
+
+            Date currentDate = new Date();
+            Calendar calendar = GregorianCalendar.getInstance();
+            calendar.setTime(currentDate);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+            JSONObject currentHourData = hourlyDataCurrentDay.getJSONObject(hour + "H00");
+            this.temperature = currentHourData.getString("TMP2m");
+            this.humidity = currentHourData.getString("RH2m");
+            this.pressure = currentHourData.getString("PRMSL");
+            this.windDirection = currentHourData.getString("WNDDIRCARD10");
+            this.condition = currentHourData.getString("CONDITION");
+            this.iconCondition = currentHourData.getString("ICON");
+        } catch (JSONException e) {
+            Log.e("WeatherClient", e.getMessage());
+        }
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getSunrise() {
+        return this.sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public String getTemperature() {
+        return this.temperature;
+    }
+
+    public String getMaxTemperature() {
+        return this.maxTemperature;
+    }
+
+    public String getMinTemperature() {
+        return this.minTemperature;
+    }
+
+    public String getHumidity() {
+        return this.humidity;
+    }
+
+    public String getPressure() {
+        return this.pressure;
+    }
+
+    public String getWindDirection() {
+        return this.windDirection;
+    }
+
+    public String getCondition() {
+        return this.condition;
+    }
+
+    public String getIconCondition() {
+        return this.iconCondition;
+    }
+
+    public String getDayShort() {
+        return this.dayShort;
+    }
+}
